83aa38019ecdaa2681565c84a008e88f0189bf4f
yosserayari021-wq <yosserayari021@gmail.com>
2026-02-09 18:34:11 +0100

    Time: 3 ms (93.05%) | Memory: 12.4 MB (89.35%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        total=0
+        prev_value=0
+        for char in reversed(s):
+            current_value=roman[char]
+            if current_value < prev_value:
+                total -= current_value  # Cas soustractif
+            else:
+                total += current_value
+                prev_value = current_value
+        return total
+
+
